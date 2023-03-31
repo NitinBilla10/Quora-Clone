69319456f5e95dadcd92d9e49599a4a3fd1e6e57
Nitin Billa <nitinbilla3@gmail.com>
2023-04-01 02:30:34 +0530

    css correction

--- src/components/Widgetcontent.js
+++ src/components/Widgetcontent.js
@@ -3,30 +3,32 @@ import React from 'react'
 function Widgetcontent() {
   return (
     <div>
+        <>
       <div class="list-group">
-  <a href="#" class="list-group-item list-group-item-action" aria-current="true">
+  <a href="/" class="list-group-item list-group-item-action" aria-current="true">
     <div class="d-flex w-100 justify-content-between">
-      <h6 class="mb-1"><img src="https://qph.cf2.quoracdn.net/main-thumb-ti-1379460-100-euizixbtrhixputbbrljnkykxdegpaha.jpeg"/>Mobile App Programmer</h6>
+      <h6 class="mb-1"><img src="https://qph.cf2.quoracdn.net/main-thumb-ti-1379460-100-euizixbtrhixputbbrljnkykxdegpaha.jpeg" alt=''/>Mobile App Programmer</h6>
     </div>
     <p class="mb-1">Latest/Upcoming Technology & Modern Mobile World (Follow Space & Keep up to date)</p>
     
   </a>
-  <a href="#" class="list-group-item list-group-item-action">
+  <a href="/" class="list-group-item list-group-item-action">
     <div class="d-flex w-100 justify-content-between">
-      <h6 class="mb-1">< img src="https://qph.cf2.quoracdn.net/main-thumb-ti-1670954-100-hnuodlzvzsyhqvdcquolsfndacllrsfe.jpeg"/> Education & Educators</h6>
+      <h6 class="mb-1">< img src="https://qph.cf2.quoracdn.net/main-thumb-ti-1670954-100-hnuodlzvzsyhqvdcquolsfndacllrsfe.jpeg" alt=''/> Education & Educators</h6>
       
     </div>
     <p class="mb-1">Education & Educators /Teachers /Teacher Trainers /Professors /Lecturers /Mentors</p>
     
   </a>
-  <a href="#" class="list-group-item list-group-item-action">
+  <a href="/" class="list-group-item list-group-item-action">
     <div class="d-flex w-100 justify-content-between">
-      <h6 class="mb-1"><img src="https://qph.cf2.quoracdn.net/main-thumb-ti-3816792-100-oaqgejkmlqzncjsyhatiebfwjitqhbue.jpeg"/>Study Abroad Club</h6>
+      <h6 class="mb-1"><img src="https://qph.cf2.quoracdn.net/main-thumb-ti-3816792-100-oaqgejkmlqzncjsyhatiebfwjitqhbue.jpeg" alt=''/>Study Abroad Club</h6>
       
     </div>
     <p class="mb-1">Mentoring & expert guidance on getting admitted to top ranked universities abroad.</p>
   </a>
 </div>
+</>
     </div>
   )
 }
